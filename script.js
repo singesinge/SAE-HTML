98d4f2ecda0e3d2f70ec6546b655bdf75f75ea01
singesinge <thomasbout662@gmail.com>
2022-12-04 00:26:32 +0100

    tentative de transition entre page bibli barbas

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function toggleNav(){
 	hamburgerButton.classList.toggle("active")
 	navigation.classList.toggle("active")
   }
+
+
+/*==animation changement page ==*/
+const wipe = document.querySelector('.wipe-transition');
+const TLAnim = new TimelineMax();
+
+barba.init({
+  sync: true,
+
+  transitions: [
+    {
+      async leave(){
+
+        TLAnim.to(wipe, {left: '0%', ease: "power2.out", duration: 0.5});
+
+        await delay(1500);
+        done();
+
+      },
+      enter(){
+
+         TLAnim
+         .to(wipe, {left: '100%', ease:"power2.in", duration: 0.5})
+         .set(wipe, {left: '-100%'})
+      }
+    }
+  ]
+
+})
