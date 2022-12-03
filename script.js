a3747c25256664ff535ff55f6c9db2f275c95163
singesinge <thomasbout662@gmail.com>
2022-12-03 16:26:14 +0100

    Ajout test page acceuil

---
+++ script.js
@@ -0,0 +1,8 @@
+/*==boutton et menu apparition==*/
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+hamburgerButton.addEventListener("click", toggleNav)
+function toggleNav(){
+	hamburgerButton.classList.toggle("active")
+	navigation.classList.toggle("active")
+  }
