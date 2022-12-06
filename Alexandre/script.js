a80d2c393307c26676f6689fbe50bf3aa236fcb8
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2022-12-06 18:12:52 +0100

    Mise à jour Alexandre

--- Alexandre/script.js
+++ Alexandre/script.js
@@ -1,22 +1,21 @@
 /* Animation H1 */
 
-  /*==Pour animerle texte de la premiere page==*/
-  const txtAnim = document.querySelector('h1');
-  new Typewriter(txtAnim, {
-    loop: true,
-    deleteSpeed: 5
-  })
-  .changeDelay(20)
-  .typeString('<span style="color: white;">Alexandre BERGERON,</span>')
-  .pauseFor(300)
-  .typeString('<span style="color: white;"></br>Etudiant en BUT informatique</span>')
-  .pauseFor(1000)
-  .typeString('<span style="color: white;"></br>Cocréateur de ce site web</span>')
-  .pauseFor(1000)
-  .typeString('<span style="color: white;"></br>Voici le parcours professionnel</span>')
-  .typeString('<span style="color: white;"></br> auquel j\'aspire.</span>')
-  .pauseFor(7000)
-  .start()
+const txtAnim = document.querySelector('h1');
+new Typewriter(txtAnim, {
+  loop: true,
+  deleteSpeed: 5
+})
+.changeDelay(20)
+.typeString('<span style="color: white;">Alexandre BERGERON,</span>')
+.pauseFor(300)
+.typeString('<span style="color: white;"></br>Etudiant en BUT informatique</span>')
+.pauseFor(1000)
+.typeString('<span style="color: white;"></br>Cocréateur de ce site web</span>')
+.pauseFor(1000)
+.typeString('<span style="color: white;"></br>Voici le parcours professionnel</span>')
+.typeString('<span style="color: white;"></br> auquel j\'aspire.</span>')
+.pauseFor(7000)
+.start()
 
 /* Menu Accueil */
 
@@ -29,3 +28,31 @@ function toggleNav(){
   hamburgerButton.classList.toggle("active")
   navigation.classList.toggle("active")
 }
+
+/* Animation H3 */
+
+const txtAnima = document.querySelector('h3');
+new Typewriter(txtAnima, {
+  loop: true,
+  deleteSpeed: 5
+})
+.changeDelay(20)
+.typeString('<span style="color: red;">Java</span>')
+.pauseFor(1000)
+.deleteChars(4)
+.typeString('<span style="color: blue;">Py</span><span style="color: yellow;">thon</span>')
+.pauseFor(1000)
+.deleteChars(6)
+.typeString('<span style="color: lightblue;">C++</span')
+.pauseFor(1000)
+.deleteChars(3)
+.typeString('<span style="color: white;">PHP</span>')
+.pauseFor(1000)
+.deleteChars(3)
+.typeString('<span style="color: yellow;">JavaScript</span>')
+.pauseFor(1000)
+.deleteChars(10)
+.typeString('<span style="color: darkblue;">SQL</span>')
+.pauseFor(1000)
+.deleteChars(3)
+.start()
